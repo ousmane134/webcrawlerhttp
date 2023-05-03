cd394822fbcfef394ab857be822278b1d376fca3
Ousmane Cissé <cisseousmane134@gmail.com>
2023-05-02 23:42:38 -0400

    geturlSfromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,6 @@
+
+const {JSDOM} = require('jsdom')
+const { relative } = require('path')
 function normalizeURL(urlString) {
 
     const urlObject = new URL(urlString)
@@ -12,6 +15,42 @@ function normalizeURL(urlString) {
         return hostPath
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+
+    for(const linkElement of linkElements) {
+        if(linkElement .href.slice(0,1) === '/') {
+            //relative
+            try {
+
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch(err) {
+                console.log(`error with relative URL: ${error.message}`)
+
+            }
+            
+        }
+        else {
+            //absolute
+            try {
+
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch(err) {
+                console.log(`error with absolute URL: ${error.message}`)
+
+            }
+        }
+        
+    }
+    return urls
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
